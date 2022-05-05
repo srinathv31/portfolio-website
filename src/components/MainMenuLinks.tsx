390ca87fc9c326dca16fdf318093dd07d0ec0cb1
Srinath Venkatesh <srinathv@udel.edu>
2022-05-04 21:48:36 -0400

    Menu created

--- src/components/MainMenuLinks.tsx
+++ src/components/MainMenuLinks.tsx
@@ -1,16 +1,23 @@
 // Source Imports
-import { Switch } from "@mui/material";
 import React from "react";
+import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Close } from "@mui/icons-material";
+import { DarkModeToggle } from "./DarkModeToggle";
 
-export default function MainMenuLinks({ menuType }: {
-    menuType: "main-page" | "side-menu"
+export default function MainMenuLinks({ menuType, setToggleDrawer }: {
+    menuType: "main-page" | "side-menu",
+    setToggleDrawer?: (t: boolean) => void
 }): JSX.Element {
     return(
         <nav id="col-2">
+            {setToggleDrawer !== undefined && 
+                <IconButton aria-label="delete" size="large" style={{ color: "white", position: "absolute", top: 20, right: 20 }} onClick={() => setToggleDrawer(false)}>
+                    <Close fontSize="inherit" />
+                </IconButton>}
             <ul className="menu-links">
-                { menuType === "side-menu" && <li>
-                    <Switch></Switch>
+                {menuType === "side-menu" && <li style={{ color: "white" }}>
+                    {"Mode: "}<DarkModeToggle onChange={()=>console.log("change mode")} sx={{ m: 1 }} defaultChecked />
                 </li>}
                 <li>
                     <Link className="App-link" to="/about">Projects</Link>
--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -3,20 +3,22 @@ import React from "react";
 import { Button, Drawer } from "@mui/material";
 import { useState } from "react";
 import MainMenuLinks from "./MainMenuLinks";
-
+import { Menu } from "@mui/icons-material";
 
 export default function MenuButton(): JSX.Element {
     const [toggleDrawer, setToggleDrawer] = useState<boolean>(false);
 
     return(
         <div>
-            <Button onClick={() => setToggleDrawer(!toggleDrawer)}>{"MENU"}</Button>
+            <Button onClick={() => setToggleDrawer(!toggleDrawer)} variant="contained" endIcon={<Menu />}>
+                {"MENU"}
+            </Button>
             <Drawer
                 anchor={"right"}
                 open={toggleDrawer}
                 onClose={() => setToggleDrawer(false)}
             >
-                {<MainMenuLinks menuType="side-menu"/>}
+                {<MainMenuLinks menuType="side-menu" setToggleDrawer={setToggleDrawer}/>}
             </Drawer>
         </div>
     );
